9e80f78842ee61a944ee008ab8f87762e2f2b360
Nathan Polomark <polomarkn@hotmail.com>
2020-11-17 20:39:56 -0800

    Routes for burger functions....incomplete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,31 @@
 const express = require('express')
-require('../models/burger.js')
+const router = express.Router()
+
+const burger = require('../models/burger.js')
+
+// Route to display page
+router.get("/", async function(req, res) {
+    const burgers = await burger.getAllBurgers()
+    
+    res.render('index', {burgers})
+});
+
+router.post('/burger/create', async function(req, res) {
+    // let allBurgers = []
+    // const burgers = await burger.getAllBurgers()
+    // console.log(`[post] looking in getAllBurgers`, burgers)
+    // allBurgers.push(burgers)
+    // console.log(`[post] pushing array`, allBurgers)
+    const createBurger = await burger.addBurger(req.body.name)
+    console.log(createBurger)
+    
+    res.send(createBurger)
+})
+
+router.put('/burger/eat/:id', async function(req, res) {
+    const devourBurger = await burger.eatBurger(req.params.id)
+    console.log(devourBurger)
+    res.send(devourBurger)
+})
+
+module.exports = router
