23cadcf5ebe7385ba8a21419fc5c82f6ceb99624
Nathan Polomark <polomarkn@hotmail.com>
2020-11-17 20:41:53 -0800

    Burger functions that controller can use

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,15 @@
 const orm = require('../config/orm.js')
+
+function getAllBurgers() {
+  return orm.selectAll('burgers');
+}
+
+function addBurger(name) {
+  return orm.insertOne('burgers', { burger_name: name });
+}
+
+function eatBurger(id) {
+  return orm.updateOne('burgers', { devoured: true }, { id });
+}
+
+module.exports = {getAllBurgers, addBurger, eatBurger}
